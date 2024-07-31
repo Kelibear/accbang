b835a6b5827fecd78b9f6ce5ad8c2487829e4663
Kelibear <falihakbar18@gmail.com>
2024-08-01 00:39:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const pages = [
   "So, dont be afraid to be yourself around me, and dont be afraid of losing yourself. You won't, even if you think it is, you are still you no matter what. <b>I want to take part in your life being the person who will always be there even if you are at your lowest.</b> Don't ever look down on yourself! You are always enough for me, <b>you are the one that i want.</b>",
   "I need you like the plants needs their Carbon Dioxide... I can't breathe if you're not near. Im sorry that you're hurting, I swear i just want to love you, and i can't do it properly... I can't keep you to stay... If loving me is hard, I apologize.",
   "You know, I wait because that's what love does, they wait. I will stay even if things get hard, you will always have a special place in my heart, You deserve all the love in the world, well i hope im your world hahaha. I do hope, because...<br><br><b>You are... my everything.</b><br><br>Thank you for being yourself. And sorry I'm not enough.<br><br>With love, Keli.<br><br><br><button onclick=\"location.href='http://www.katysdaisies.com/share/UmhlaW5hfEtlbGl8SSdtIFNvcnJ5LApJIExvdmUgWW91LnxDcnkgQWJvdXQgSXQgTGF0ZXI6IFNoYW1lbGVzcyBTdW1tZXJ8NDM=/';\">For You, My Love</button>",
-  "\<blank\><br><br>I leave this page blank, incase our story continues."
+  "\[blank\]<br><br>I leave this page blank, incase our story continues."
 ];
 let currentPage = 0;
 
