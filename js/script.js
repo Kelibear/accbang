b1932a6caed8b586e8bbaa3c2a329c7ec293b34a
Kelibear <falihakbar18@gmail.com>
2025-02-25 15:30:03 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 const bg_music = document.getElementById('bg_music');
 
-function start() {
-    let bg_music = document.getElementById('bg_music');
-    bg_music.play().catch(error => console.log("Autoplay blocked:", error));
-}
-
-document.getElementById("heart").addEventListener("click", start);
-
+  let heart = document.getElementById("heart");
+  heart.addEventListener("transitionend", () => {
+    bg_music.play();
+});
   bg_music.addEventListener('ended', () => {
     bg_music.play();
 });
